1869edc18d55dd673756224830084edfd8c22e87
Daniel Klomp <danielklomp@live.nl>
2016-04-21 15:15:50 +0200

    --

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,8 +1,6 @@
 int sensorPin = A0;    // select the input pin for the potentiometer
 int setAlarm = 8;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
-int numberOfDetections = 0;
-bool activateAlarm = true;
 
 unsigned int minute = 60 * 1000; // 60 * (1000 milliseconds)
 unsigned int previousMillis = 0;
@@ -15,45 +13,49 @@ void setup() {
 }
 
 void loop() {
+  int x = 1;
+  int numberOfDetections = 0;
   
-  currentMillis = millis();
-  sensorValue = analogRead(sensorPin);
-  
-  Serial.print(sensorValue);
-  Serial.print("\n");
-  
-  if (activateAlarm == true){
-    digitalWrite(setAlarm, LOW);
-    delay(500);
-    digitalWrite(setAlarm, HIGH);
-    activateAlarm = false;
-  }
-  
-  if (sensorValue >= 500){
-    numberOfDetections++;
-    if(numberOfDetections == 1) {
-      previousMillis = currentMillis; // Reset timer
+  while(true){
+    currentMillis = millis();
+    sensorValue = analogRead(sensorPin);
+    
+    Serial.print(sensorValue);
+    Serial.print("\n");
+    
+    if (x != 1){
+      digitalWrite(setAlarm, LOW);
+      delay(500);
+      digitalWrite(setAlarm, HIGH);
+      x = 1;
     }
-  }
-  
-  /*
-   * Check if there have been 10 or more detections
-   */
-  if(numberOfDetections >= 10){
-     Serial.print("Het nummer van bewegingen is: ");
-     Serial.print(numberOfDetections);
-     Serial.print("\n");
-     numberOfDetections = 0;
-     previousMillis = currentMillis; // Reset timer
-  }
+    
+    if (sensorValue >= 500){
+      numberOfDetections++;
+      if(numberOfDetections == 1) {
+        previousMillis = currentMillis; // Reset timer
+      }
+    }
+    
+    /*
+     * Check if there have been 10 or more detections
+     */
+    if(numberOfDetections >= 10){
+       Serial.print("Het nummer van bewegingen is: ");
+       Serial.print(numberOfDetections);
+       Serial.print("\n");
+       numberOfDetections = 0;
+       previousMillis = currentMillis; // Reset timer
+    }
 
-  /*
-   * If the timer passed 1 minute, reset the timer and detection counter
-   */
-  if((currentMillis - previousMillis) >= minute) {
-    Serial.print("Reset de timer");
-    previousMillis = currentMillis; // Reset timer
-    numberOfDetections = 0;
+    /*
+     * If the timer passed 1 minute, reset the timer and detection counter
+     */
+    if((currentMillis - previousMillis) >= minute) {
+      Serial.print("Reset de timer");
+      previousMillis = currentMillis; // Reset timer
+      numberOfDetections = 0;
+    }
+    delay(2000);
   }
-  delay(2000);
 }
