0cb7b90cf7e0006f59c6654c63f3cd12abfb97c4
Aydin Biber <biber.aydin@gmail.com>
2016-04-21 12:35:04 +0200

    Added Serial.print to check timer

--- src/main/main.ino
+++ src/main/main.ino
@@ -2,8 +2,9 @@ int sensorPin = A0;    // select the input pin for the potentiometer
 int setAlarm = 8;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
 
-#define minute (60 * 1000); // 60 * (1000 milliseconds)
-unsigned long previousMillis = 0;
+unsigned long int minute (60 * 1000); // 60 * (1000 milliseconds)
+unsigned long int previousMillis = 0;
+unsigned long int currentMillis = 0;
 
 void setup() {
   // declare the ledPin as an OUTPUT:
@@ -14,32 +15,35 @@ void setup() {
 void loop() {
   int x = 1;
   int numberOfDetections = 0;
-  unsigned long currentMillis = millis();
   
   while(true){
+    currentMillis = millis();
     sensorValue = analogRead(sensorPin);
+    
     Serial.print(sensorValue);
     Serial.print("\n");
+    
     if (x != 1){
       digitalWrite(setAlarm, LOW);
       delay(500);
       digitalWrite(setAlarm, HIGH);
       x = 1;
     }
+    
     if (sensorValue >= 500){
       numberOfDetections++;
       if(numberOfDetections == 1) {
         previousMillis = currentMillis; // Reset timer
       }
     }
-
+    
     /*
      * Check if there have been 10 or more detections
      */
     if(numberOfDetections >= 10){
-       Serial.print("Het nummer van bewegingen is: ");
-       Serial.print(numberOfDetections);
-       Serial.print("\n");
+       //Serial.print("Het nummer van bewegingen is: ");
+       //Serial.print(numberOfDetections);
+       //Serial.print("\n");
        numberOfDetections = 0;
        previousMillis = currentMillis; // Reset timer
     }
@@ -47,10 +51,17 @@ void loop() {
     /*
      * If the timer passed 1 minute, reset the timer and detection counter
      */
-    if(currentMillis - previousMillis >= minute) {
+    if((currentMillis - previousMillis) >= minute) {
+      Serial.print("Reset de timer");
       previousMillis = currentMillis; // Reset timer
       numberOfDetections = 0;
     }
+
+    Serial.print("\nCurrent: ");
+    Serial.print(currentMillis);
+    Serial.print("\nPrevious: ");
+    Serial.print(previousMillis);
+    Serial.print("\n");
     delay(2000);
   }
 }
