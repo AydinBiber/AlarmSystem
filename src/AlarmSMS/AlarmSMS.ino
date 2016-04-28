abb53d007ee56be8c09d68abac13de90dc0c7510
jop van buuren <jopvanbuuren@live.nl>
2016-04-28 15:01:12 +0200

    finished product of Motion+SMS

--- src/AlarmSMS/AlarmSMS.ino
+++ src/AlarmSMS/AlarmSMS.ino
@@ -1,6 +1,12 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(2, 3);
+
+String pin= "xxxx"; 
+String number = "06xxxxxxxx";
+
 int sensorPin = A0;    // select the input pin for the potentiometer
 int setAlarm = 8;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
+int sensorValue = 0;   // variable to store the value coming from the sensor
 
 unsigned int minute = 60 * 1000; // 60 * (1000 milliseconds)
 unsigned int previousMillis = 0;
@@ -9,7 +15,33 @@ unsigned int currentMillis = 0;
 void setup() {
   // declare the ledPin as an OUTPUT:
   pinMode(setAlarm, OUTPUT);
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(9600);
+  if(pin[0] == 'x' || number[0] == 'x'){
+    Serial.println("Pin/number is not set!");
+  }
+  delay(1000);
+  mySerial.println("AT+CPIN=" + pin );
+  Serial.println("Number is set on: " + number);  
+}
+
+void sendMessage(String msg){
+  if(msg.length() > 150){
+    Serial.println("Message too long!");
+    msg = "The alarm tried to send a message that was to long!";
+  }
+  
+  mySerial.println("AT+CMGF=1");
+  while(!mySerial.available()){
+    delay(10);
+  }
+  mySerial.println("AT+CMGS=" + number);  
+  delay(400);
+  mySerial.println(msg + "\x1a");
+  //debug data
+  Serial.println("Message sent: " + msg);
 }
 
 void loop() {
@@ -44,6 +76,8 @@ void loop() {
        Serial.print("Het nummer van bewegingen is: ");
        Serial.print(numberOfDetections);
        Serial.print("\n");
+       String message = "The alarm detected: " + String(numberOfDetections) + " movements";
+       sendMessage(message);
        numberOfDetections = 0;
        previousMillis = currentMillis; // Reset timer
     }
